243972f1a07652c37ea4daeb8aa0c8bad2830eba
ekerzuleyha <66801741+ekerzuleyha@users.noreply.github.com>
2023-07-07 21:59:16 +0300

    done

--- Main.java
+++ Main.java
@@ -1,6 +1,166 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        ManagementOfPhoneApp management =new ManagementOfPhoneApp();
+
+        Scanner input = new Scanner(System.in);
+        int choice = 0;
+
+        ManagementOfPhoneApp manager = new ManagementOfPhoneApp();
+
+
+        System.out.println("Giriş yapabilmek için telefon ekleyiniz.");
+
+        System.out.println("--------------------------------");
+
+        System.out.print("Eklenecek telefonun markasını giriniz: ");
+        String marka = input.nextLine();
+        //input.nextLine();
+
+        System.out.print("Eklenecek telefonun modelini giriniz: ");
+        String model = input.nextLine();
+
+        System.out.print("Eklenecek telefonun seri numarasını giriniz: ");
+
+        String numara = input.nextLine();
+
+        System.out.print("Eklenecek telefonun depolama alanını giriniz: ");
+        String depolamaAlanı = input.nextLine();
+
+        System.out.print("Eklenecek telefonun işletim sistemini giriniz: ");
+        String sistem = input.nextLine();
+
+
+        Phone phone = new Phone(marka, model, numara, depolamaAlanı, sistem);
+        manager.addPhone(phone);
+
+        System.out.println("--------------------------------");
+        printMenu();
+
+
+        do {
+            System.out.print("Seçiminiz: ");
+            choice = input.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("--------------------------------");
+
+                    System.out.print("Eklenecek kişinin ismini giriniz: ");
+                    String isim=input.nextLine();
+                    input.nextLine();
+
+                    System.out.print("Eklenecek kişinin soyismini giriniz: ");
+                    String soyisim = input.nextLine();
+
+                    System.out.print("Eklenecek kişinin telefon numarasını giriniz: ");
+
+                    String telno = input.nextLine();
+
+                    System.out.print("Eklenecek kişinin mailini giriniz: ");
+                    String mail = input.nextLine();
+
+
+                    Person person=new Person(isim,soyisim,telno,mail);
+                    manager.addPerson(person);
+
+
+                    System.out.println("--------------------------------");
+                    break;
+
+                }
+                case 2 -> {
+                    System.out.println("--------------------------------");
+
+                    System.out.print("Eklenecek uygulamanın adını giriniz: ");
+                    String ad=input.nextLine();
+                    input.nextLine();
+
+                    System.out.print("Eklenecek uygulamanın versiyonunu giriniz: ");
+                    String version = input.nextLine();
+
+                    System.out.print("Eklenecek uygulamanın açıklamasını giriniz: ");
+
+                    String aciklama = input.nextLine();
+
+                    Application app =new Application(ad,version,aciklama);
+                    manager.addApp(app);
+
+
+                    System.out.println("--------------------------------");
+                    break;
+
+
+                }
+                case 3 -> {
+                    input.nextLine();
+                    System.out.println("Silinecek kişinin adını giriniz;");
+                    String ad = input.nextLine();
+
+                    manager.deletePerson(ad);
+
+
+                    break;
+                }
+
+                case 4 -> {
+                    input.nextLine();
+                    manager.listPersons();
+
+                    break;
+                }
+                case 5 -> {
+                    input.nextLine();
+
+                    break;
+                }
+                case 6 -> {
+                    input.nextLine();
+
+                    break;
+                }
+
+
+                case -1 -> {
+                    System.out.println("İyi günler");
+                }
+                default -> printMenu();
+            }
+        } while (choice != -1);
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*ManagementOfPhoneApp management =new ManagementOfPhoneApp();
         //management.addPerson(new Person("züleyha","eker","22222222","zuleyha@gmail.com"));
        // management.addPhone(new Phone("apple","14 pro","11111","128 GB","IOS"));
        // management.addApplication(new Application("wp","12.0","chat application"));
@@ -21,6 +181,29 @@ public class Main {
 
          management.deletePerson(person1);
          management.listPersons();
+
+          /*manager.addPerson(new Person("züleyha","eker","11111","@gmail"));
+        manager.listPersons();
+
+        manager.addApp(new Application("wp","12.0","uygulama"));
+        manager.listApp();
+
+        manager.addPhone(new Phone("samsung","a54","222222","124gb","android"));
+        manager.listPhone();*/
+
+
+
+    private static void printMenu() {
+        System.out.println("##### Menu #####");
+        System.out.println("1: Kişi ekleme");
+        System.out.println("2: Uygulama ekleme");
+        System.out.println("3: Kişi Silme");
+        System.out.println("4: Kişileri listeleme");
+        /*System.out.println("5: Filmleri Listeleme");
+        System.out.println("6: Film Arama");
+        System.out.println("7: Film Silme");
+        System.out.println("8: Film Ekleme");*/
+        System.out.println("-1: Çıkış \n\n");
 
 
     }
--- ManagementOfPhoneApp.java
+++ ManagementOfPhoneApp.java
@@ -1,12 +1,78 @@
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.*;
 
 public class ManagementOfPhoneApp {
-    public List<Application> applications =new ArrayList<>();
+    //public List<Application> applications =new ArrayList<>();
     //private Map<String, Application> applications;
-    public List<Person> persons = new ArrayList<>();
+    //public List<Person> persons = new ArrayList<>();
    // private Map<String, Person> persons;
-    public List<Phone> phones =new ArrayList<>();
+   // public List<Phone> phones =new ArrayList<>();
     //private Map <String, Phone > phones;
+
+    private Map<String ,Application> apps;
+    private Map<String ,Person> persons;
+    private Map <String,Phone> phones;
+
+
+
+    public ManagementOfPhoneApp(){
+        apps= new HashMap<>();
+        persons =new HashMap<>();
+        phones=new HashMap<>();
+
+    }
+    public void addPerson(Person person){
+        persons.put(person.getName(),person);
+        System.out.println("kullanıcı eklendi.");
+        listPersons();
+
+    }
+
+    public void deletePerson(String name){
+        persons.remove(name);
+        System.out.println("Kullanıcı silindi. ");
+        //listPersons();
+
+    }
+
+    public void addApp(Application app){
+        apps.put(app.getName(),app);
+        System.out.println("uygulama eklendi");
+        listApp();
+    }
+
+    public void addPhone(Phone phone){
+        phones.put(phone.getBrand(),phone);
+        System.out.println("telefon eklendi");
+        listPhone();
+    }
+
+
+
+
+
+    public void listPersons(){
+        for (Person person : persons.values()){
+            System.out.println(person);
+        }
+
+    }
+
+
+    public void listApp(){
+        for (Application app : apps.values()){
+            System.out.println(app);
+        }
+
+    }
+
+    public void listPhone(){
+        for (Phone phone : phones.values()){
+            System.out.println(phone);
+        }
+
+    }
+
 
     /*public ManagementOfPhoneApp(Map<String, Application> applications, Map<String, Person> persons, Map <String, Phone > phones) {
         applications = new HashMap<>();
@@ -22,7 +88,7 @@ public class ManagementOfPhoneApp {
     }*/
 
 
-    public void addPhone(Phone phone){
+   /* public void addPhone(Phone phone){
         phones.add(phone);
     }
     public void deletePhone(Phone phone){
@@ -48,31 +114,7 @@ public class ManagementOfPhoneApp {
     }
     public void updateApp(Application App){
 
-    }
-
-
-    public void listPersons(){
-        for (Person person : persons){
-            System.out.println(person);
-        }
-
-    }
-
-    public void listPhone(){
-        for (Phone phone : phones){
-            System.out.println(phone);
-        }
-
-    }
-
-
-    public void listApp(){
-        for (Application app : applications){
-            System.out.println(app);
-        }
-
-    }
-
+    }*/
 
 
 
